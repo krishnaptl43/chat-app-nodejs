8bf450ab27a0df80e2dc24891908c9f1052277ef
krishnaptl43 <krishnaptl43@gmail.com>
2025-10-04 19:34:25 +0530

    Refactor socket.io setup and improve MongoDB connection in app.js and www

--- app.js
+++ app.js
@@ -13,20 +13,14 @@ const Message = require('./model/message.model');
 
 const app = express();
 
-const server = createServer(app)
-// Setup socket.io server
-const io = new Server(server, {
-  cors: {
-    origin: "*", // React frontend
-    methods: ["GET", "POST"],
-  },
-});
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST"]
+}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -65,22 +59,59 @@ app.use(function (err, req, res, next) {
 
 const onlineUsers = new Map(); // userId -> socket.id
 
+const server = createServer(app)
+// Setup socket.io server
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // React frontend
+    methods: ["GET", "POST"],
+  },
+});
+
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
   socket.on('join', (userId) => {
     socket.userId = userId;
-    onlineUsers.set(userId, socket.id);
     socket.join(userId);
+    onlineUsers.set(userId, socket.id);
+  });
 
-    // Notify all clients about online users
-    io.emit('onlineUsers', Array.from(onlineUsers.keys()));
+  // Notify all clients about online users
+  socket.on('onlineUsers', async ({ sender, receiver }) => {
+    let isOnline = Array.from(onlineUsers.keys()).find(item => item.userId === receiver)
+    io.to(sender).to(receiver).emit("onlineUsers", isOnline)
   });
 
   socket.on('privateMessage', async ({ sender, receiver, text }) => {
     const newMessage = new Message({ sender, receiver, text });
     await newMessage.save();
     io.to(sender).to(receiver).emit('privateMessage', newMessage);
+  });
+
+  // Call request
+  socket.on("call-user", ({ to, offer, callType, fromUser }) => {
+    io.to(to).emit("incoming-call", {
+      from: socket.userId,
+      fromUser,
+      offer,
+      callType,
+    });
+  });
+
+  // Answer (accept)
+  socket.on("accept-call", ({ to, answer }) => {
+    io.to(to).emit("call-accepted", { answer });
+  });
+
+  // Reject
+  socket.on("reject-call", ({ to }) => {
+    io.to(to).emit("call-rejected");
+  });
+
+  // ICE exchange
+  socket.on("ice-candidate", ({ to, candidate }) => {
+    io.to(to).emit("ice-candidate", { candidate });
   });
 
   socket.on('disconnect', () => {
@@ -90,7 +121,11 @@ io.on('connection', (socket) => {
       console.log('User disconnected:', socket.userId);
     }
   });
+
 });
+
+console.log(onlineUsers);
+
 
 
 module.exports = server;
